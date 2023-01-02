5c479b682b895184a9ec367589b0562ac005acc1
abhinavbhashyam <84284109+abhinavbhashyam@users.noreply.github.com>
2023-01-02 15:26:02 -0600

    asdad

--- QuizzingApp/app/src/main/java/com/app/QuizzingApp/QuestionMakerActivity.java
+++ QuizzingApp/app/src/main/java/com/app/QuizzingApp/QuestionMakerActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -52,48 +53,57 @@ public class QuestionMakerActivity extends AppCompatActivity {
     public void submitQuestion(View v) {
         // question attributes
         String name = qName.getText().toString();
-        int diff = difficulty.getNumStars();
+        int diff = (int) difficulty.getRating();
 
         // answer attributes
         boolean c1 = correct1.isChecked();
         String t1 = answerText1.getText().toString();
-        boolean c2 = correct1.isChecked();
-        String t2 = answerText1.getText().toString();
-        boolean c3 = correct1.isChecked();
-        String t3 = answerText1.getText().toString();
-        boolean c4 = correct1.isChecked();
-        String t4 = answerText1.getText().toString();
+        boolean c2 = correct2.isChecked();
+        String t2 = answerText2.getText().toString();
+        boolean c3 = correct3.isChecked();
+        String t3 = answerText3.getText().toString();
+        boolean c4 = correct4.isChecked();
+        String t4 = answerText4.getText().toString();
 
+        // check blank
+        if (t1.isEmpty() || t2.isEmpty() || t3.isEmpty() || t4.isEmpty() || name.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Fill in all fields!", Toast.LENGTH_SHORT).show();
+        } else {
+            // create our answers and question
+            // question
+            Question q = new Question(name, diff);
+            // answers
+            Answer a1 = new Answer(t1, c1);
+            Answer a2 = new Answer(t2, c2);
+            Answer a3 = new Answer(t3, c3);
+            Answer a4 = new Answer(t4, c4);
+            // add answers to question
+            q.addAnswers(new ArrayList<Answer>(Arrays.asList(a1, a2, a3, a4)));
 
-        // create our answers and question
-        // question
-        Question q = new Question(name, diff);
-        // answers
-        Answer a1 = new Answer(t1, c1);
-        Answer a2 = new Answer(t2, c2);
-        Answer a3 = new Answer(t3, c3);
-        Answer a4 = new Answer(t4, c4);
-        // add answers to question
-        q.addAnswers(new ArrayList<Answer>(Arrays.asList(a1, a2, a3, a4)));
+            // submit as a collection to current ID document
+            FirebaseFirestore db = firebaseHelper.getmdb();
+            String uid = firebaseHelper.getmAuth().getCurrentUser().getUid();
+            int qid = (int) (Math.random() * 1000000);
 
-        // submit as a collection to current ID document
-        FirebaseFirestore db = firebaseHelper.getmdb();
-        String uid = firebaseHelper.getmAuth().getCurrentUser().getUid();
-        int qid = (int) (Math.random() * 1000000);
+            db.collection("Users").document(uid).collection("Questions").document(Integer.toString(qid))
+                    .set(q)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Log.i("TAG", "question added");
+                            Toast.makeText(getApplicationContext(), "Question added successfully!", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("TAG", "Error adding question", e);
+                            Toast.makeText(getApplicationContext(), "Error adding question", Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-        db.collection("Users").document(uid).collection("Questions").document(Integer.toString(qid))
-                .set(q)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Log.i("TAG", "question added");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("TAG", "Error adding question", e);
-                    }
-                });
-
+        }
     }
+
+
+
 }
